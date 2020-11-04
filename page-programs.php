1d91ae406ae66279bcbe98fedf4e7bc1a81dce9b
Darsh <darsh2987@gmail.com>
2020-11-04 11:34:51 +0000

    program page created

---
+++ page-programs.php
@@ -0,0 +1,14 @@
+<?php /* Template Name: Programs */?>
+
+<?php get_header(); ?>
+
+<?php $backgroundBanner = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), "full"); ?>
+<section class="page-banner">
+  <div class="page-banner--background-image" style="background-image: url(<?php echo $backgroundBanner["0"]; ?>)"></div>
+  <div class="page-banner--content page-banner--content-template section-width">
+    <h1 class="title">All <?php the_title(); ?></h1>
+    <h2 class="headline"><?php the_field("sub_title"); ?></h2>
+  </div>
+</section>
+
+<?php get_footer(); ?>
