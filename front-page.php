582c8c32f63b78a3c69554ff644f56cf42737233
Darsh <darsh2987@gmail.com>
2020-11-05 12:50:10 +0000

    removed duplicated event summary markup and created templated part

--- front-page.php
+++ front-page.php
@@ -36,37 +36,9 @@
       ));
 
       while($homepageEvents->have_posts()) {
-        $homepageEvents->the_post(); ?>
-          <div class="post-summary">
-            <a class="post-summary--date" href="<?php the_permalink(); ?>">
-              <span class="post-summary--month">
-                <?php 
-                  $eventDate = new DateTime(get_field("event_date")); 
-                  echo $eventDate->format("M")
-                ?>
-              </span>
-              <span class="post-summary--day">
-                <?php 
-                  $eventDate = new DateTime(get_field("event_date")); 
-                  echo $eventDate->format("d")
-                ?>
-              </span>
-            </a>
-            <div class="post-summary--content">
-              <h5 class="post-summary--title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-              <p class="post-summary--excerpt">
-                <?php 
-                  if(has_excerpt()) {
-                    echo get_the_excerpt();
-                  } else {
-                    echo wp_trim_words(get_the_content(), 15);
-                  }
-                ?>
-                <a class="read-learn-more" href="<?php the_permalink(); ?>">Learn More</a>
-              </p>
-            </div>
-          </div>
-      <?php }
+        $homepageEvents->the_post();
+          get_template_part("template-parts/event", "summary");
+      }
     ?>
     <?php $eventsPage = get_page_by_title("Events");?>
     <button><a href="<?php echo get_permalink($eventsPage->ID); ?>">View All Events</a></button>
--- page-past-events.php
+++ page-past-events.php
@@ -35,37 +35,9 @@
     ));
 
     while($pastEvents->have_posts()) {
-      $pastEvents->the_post(); ?>
-        <div class="post-summary">
-          <a class="post-summary--date" href="<?php the_permalink(); ?>">
-            <span class="post-summary--month">
-              <?php 
-                $eventDate = new DateTime(get_field("event_date")); 
-                echo $eventDate->format("M")
-              ?>
-            </span>
-            <span class="post-summary--day">
-              <?php 
-                $eventDate = new DateTime(get_field("event_date")); 
-                echo $eventDate->format("d")
-              ?>
-            </span>
-          </a>
-          <div class="post-summary--content">
-            <h5 class="post-summary--title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-            <p class="post-summary--excerpt">
-              <?php 
-                if(has_excerpt()) {
-                  echo get_the_excerpt();
-                } else {
-                  echo wp_trim_words(get_the_content(), 15);
-                }
-              ?>
-              <a class="read-learn-more" href="<?php the_permalink(); ?>">Learn More</a>
-            </p>
-          </div>
-        </div>
-    <?php }
+      $pastEvents->the_post();
+         get_template_part("template-parts/event", "summary");
+   }
   ?>
 
   <?php
