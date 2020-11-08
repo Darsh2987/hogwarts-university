ee36ff47ae1c06419edf3555a135caa799d3b69a
Darsh <darsh2987@gmail.com>
2020-11-08 19:09:25 +0000

    404 page markup and styles

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<section class="page-banner">
+<div class="page-banner--background-image" style="background-image: url(<?php echo get_theme_file_uri("assets/images/hogwarts-banner.png"); ?>)"></div>
+  <div class="page-banner--content">
+    <h1 class="headline">404 Error!</h1>
+  </div>
+</section>
+
+<section class="section-width error-page">
+  <p>Oops, Sorry! Looks like theres been an error. That page or post either could not be found or does not exist. <br><br>Please try search or head back <a href="<?php echo site_url("/home")?>">Home</a>. </p>
+</section>
+
+
+
+  
+<?php get_footer(); ?>
